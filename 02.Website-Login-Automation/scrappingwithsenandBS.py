546438318a0d8963ba944fa7a048eea60e3ef4aa
Isaac Ayanda <isaacayanda@gmail.com>
2024-03-15 05:03:13 -0500

    added currency pairs

--- 02.Website-Login-Automation/scrappingwithsenandBS.py
+++ 02.Website-Login-Automation/scrappingwithsenandBS.py
@@ -1,2 +1,7 @@
 from bs4 import BeautifulSoup
 
+def get_currency(input_currency, output_currency):
+    url = f'https://www.x-rates.com/calculator/?from={input_currency}&to={output_currency}&amount=1'
+    print(url)
+
+get_currency('INR', 'USD')
