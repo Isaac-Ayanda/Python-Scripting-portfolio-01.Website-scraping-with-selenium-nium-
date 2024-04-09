c54537ef3bd2512526d53ce82ec64a03bae4d05c
Isaac Ayanda <isaacayanda@gmail.com>
2024-04-09 07:10:55 -0500

    updated file

--- 03.Scrapping-with-rest-API/rest-api.py
+++ 03.Scrapping-with-rest-API/rest-api.py
@@ -1,6 +1,6 @@
 import requests
 
-r = requests.get('https://newsapi.org/v2/everything?qInTitle=stock%20market&from=2022-2-27&to=2022-2-28&sortBy=popularity&language=en&apiKey=890603a55bfa47048e4490069ebee18c')
+r = requests.get('https://newsapi.org/v2/everything?qInTitle=united%20states&from=2024-3-9&to=2024-3-27&sortBy=popularity&language=en&apiKey=890603a55bfa47048e4490069ebee18c')
 content = r.json()
 print(content)
 print(type(content))
