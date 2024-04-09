d74a365b8ad4396698d730bcef86ecc8314e9c48
Isaac Ayanda <isaacayanda@gmail.com>
2024-04-09 06:53:42 -0500

    working on the script

--- 03.Scrapping-with-rest-API/rest-api.py
+++ 03.Scrapping-with-rest-API/rest-api.py
@@ -3,3 +3,4 @@ import requests
 r = requests.get('https://newsapi.org/v2/everything?qInTitle=stock%20market&from=2022-2-27&to=2022-2-28&sortBy=popularity&language=en&apiKey=890603a55bfa47048e4490069ebee18c')
 content = r.json()
 print(content)
+print(type(content))
