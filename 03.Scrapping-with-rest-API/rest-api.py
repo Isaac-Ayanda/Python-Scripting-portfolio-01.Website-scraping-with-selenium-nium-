8974208e5da1c093da2cd09e17cbe7c4b7f3eecd
Isaac Ayanda <isaacayanda@gmail.com>
2024-04-09 07:16:23 -0500

    getting values of keys

--- 03.Scrapping-with-rest-API/rest-api.py
+++ 03.Scrapping-with-rest-API/rest-api.py
@@ -2,5 +2,6 @@ import requests
 
 r = requests.get('https://newsapi.org/v2/everything?qInTitle=united%20states&from=2024-3-9&to=2024-3-27&sortBy=popularity&language=en&apiKey=890603a55bfa47048e4490069ebee18c')
 content = r.json()
-print(content)
-print(type(content))
+# print(content)
+# print(type(content))
+print(content['articles'][0]['description'])
