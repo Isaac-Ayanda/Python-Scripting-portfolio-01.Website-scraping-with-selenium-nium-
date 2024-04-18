36b5834fb568454c9206482899998fecbbc0f2c1
Isaac Ayanda <isaacayanda@gmail.com>
2024-04-18 06:16:59 -0500

    working on API

--- 03.Scrapping-with-rest-API/rest-api.py
+++ 03.Scrapping-with-rest-API/rest-api.py
@@ -1,6 +1,6 @@
 import requests
 
-def get_news(language='en', 
+def get_news(country language='en', 
              api_key='aa27ff3681d34f0b88cfabe10e30dcac'): url = f'https://newsapi.org/v2/everything?qInTitle={topic}&from={from_date}&to={to_date}8&sortBy=popularity&language={language}&apiKey={api_key}'
 
 r = requests.get(url)
