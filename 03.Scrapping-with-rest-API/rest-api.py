b4e79a3cd6f5c1527fd36343130a6f5948c846cf
Isaac Ayanda <isaacayanda@gmail.com>
2024-04-23 06:44:34 -0500

    new scripts

--- 03.Scrapping-with-rest-API/rest-api.py
+++ 03.Scrapping-with-rest-API/rest-api.py
@@ -1,13 +1,9 @@
 import requests
 
-def get_weather(city, units='metrics', api_key='0051bfbc99fb4c3a9ce9bf684b8edd48'): url = f'https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units={units}'
-  r = requests.get(url)
-  content = r.json()
-  articles = content['articles']
-  results = []
-  for article in articles:
-        results.append(f"TITLE\n'{article['title']}, '\nDESCRIPTION\n', {article['description']}")
-  return results
-
+def get_weather(city, units='metrics', api_key='0051bfbc99fb4c3a9ce9bf684b8edd48'): 
+    url = f'https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units={units}'
+    r = requests.get(url)
+    content = r.json()
+    return content
 
 print(get_news(country='us'))
