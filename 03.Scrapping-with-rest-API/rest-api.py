803a49ebb9528807dc0069c9e8ad3885ab3392be
Isaac Ayanda <isaacayanda@gmail.com>
2024-04-23 06:38:14 -0500

    openweather2

--- 03.Scrapping-with-rest-API/rest-api.py
+++ 03.Scrapping-with-rest-API/rest-api.py
@@ -1,6 +1,6 @@
 import requests
 
-def get_weather(city, units='metrics', api_key='0051bfbc99fb4c3a9ce9bf684b8edd48'): url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={api_key}'
+def get_weather(city, units='metrics', api_key='0051bfbc99fb4c3a9ce9bf684b8edd48'): url = f'https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}&units={units}'
   r = requests.get(url)
   content = r.json()
   articles = content['articles']
