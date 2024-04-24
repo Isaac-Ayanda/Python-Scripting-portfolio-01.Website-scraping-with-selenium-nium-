2b3970616787a4af63bf85121ce90246cc0d4a3b
Isaac Ayanda <isaacayanda@gmail.com>
2024-04-24 07:08:49 -0500

    updated

--- 03.Scrapping-with-rest-API/rest-api.py
+++ 03.Scrapping-with-rest-API/rest-api.py
@@ -4,7 +4,8 @@ def get_weather(city, units='metrics', api_key='0051bfbc99fb4c3a9ce9bf684b8edd48
     url = f'https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units={units}'
     r = requests.get(url)
     content = r.json()
-    for dicty in content['list']
+    for dicty in content['list']:
+        print(dicty['dt_text'])
     return content
 
 print(get_weather(city='washington'))
