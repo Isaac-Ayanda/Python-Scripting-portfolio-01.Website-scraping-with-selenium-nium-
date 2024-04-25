ac8142fb77323594736895c3d1d9d2c2e8792e51
Isaac Ayanda <isaacayanda@gmail.com>
2024-04-25 06:50:04 -0500

    api done

--- 03.Scrapping-with-rest-API/rest-api.py
+++ 03.Scrapping-with-rest-API/rest-api.py
@@ -4,9 +4,9 @@ def get_weather(city, units='metric', api_key='0051bfbc99fb4c3a9ce9bf684b8edd48'
     url = f'https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units={units}'
     r = requests.get(url)
     content = r.json()
-    with open('data.txt', 'a')
-    for dicty in content['list']:
-        print(dicty['dt_txt'], dicty['main']['temp'],dicty['weather'][0]['description'])
-    # return content
+    with open('data.txt', 'a') as file:
+        for dicty in content['list']:
+            file.write(f"dicty['dt_txt'], dicty['main']['temp'],dicty['weather'][0]['description']")
+        # return content
 
 print(get_weather(city='washington'))
