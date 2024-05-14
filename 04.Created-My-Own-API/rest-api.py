8c3132dd09cc2097fbd983ca91c17f8d35cb1f88
Isaac Ayanda <isaacayanda@gmail.com>
2024-05-14 06:51:13 -0500

    updated script'

--- 04.Created-My-Own-API/rest-api.py
+++ 04.Created-My-Own-API/rest-api.py
@@ -7,5 +7,8 @@ app = Flask(__name__)
 def home():
     return '<h1>Currency Rate API</h1> <p> Example URL: /api/vi/usd-eur</p>'
 
-@app.route('api/v1/usd-eur')
+@app.route('api/v1/<in_cur>-<out_cur>')
+def api(in_cur, out_cur):
+
+
 app.run(host='0.0.0.0')
