fc4938ea7c953c12a42fbb9ee9e8b1e192985a34
Isaac Ayanda <isaacayanda@gmail.com>
2024-05-16 07:22:06 -0500

    update file

--- 04.Created-My-Own-API/rest-api.py
+++ 04.Created-My-Own-API/rest-api.py
@@ -3,11 +3,12 @@ from flask import Flask
 from bs4 import BeautifulSoup
 import requests
 
-def get_currency(in_currency, out_crrency)
 
 def get_currency(in_currency, out_currency):
     url = f'https://www.x-rates.com/calculator/?from={in_currency}&to={out_currency}&amount=1'
-    content = requests
+    content = requests.get(url).text
+    soup = BeautifulSoup(content, 'html.parser')
+    rate =soup.find("span")
 
 app = Flask(__name     __)
 
