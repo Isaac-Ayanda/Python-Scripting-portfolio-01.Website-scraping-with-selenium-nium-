ddfa0610813efdcf87db2b79d7cb4e08c9019a2e
Isaac Ayanda <isaacayanda@gmail.com>
2024-05-16 07:24:44 -0500

    updated script

--- 04.Created-My-Own-API/rest-api.py
+++ 04.Created-My-Own-API/rest-api.py
@@ -8,7 +8,9 @@ def get_currency(in_currency, out_currency):
     url = f'https://www.x-rates.com/calculator/?from={in_currency}&to={out_currency}&amount=1'
     content = requests.get(url).text
     soup = BeautifulSoup(content, 'html.parser')
-    rate =soup.find("span")
+    rate =soup.find("span", class_="ccOutputRslt").get_text()
+
+    return rate
 
 app = Flask(__name     __)
 
